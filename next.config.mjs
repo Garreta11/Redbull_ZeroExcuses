17733b566e570fdd07dae31da0f6ad2bbc63911c
Jordi Garreta <jordigarreta11@gmail.com>
2024-10-10 09:58:22 +0200

    ignore errors on build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    /**
+     * Linting and building will be done in separate steps in the build
+     * pipeline, so it's preferable to know whether each step passes independent
+     * of the other.
+     */
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    /**
+     * Typechecking and building will be done in separate steps in the build
+     * pipeline, so it's preferable to know whether each step passes independent
+     * of the other.
+     */
+    ignoreBuildErrors: true,
+  },
+  productionBrowserSourceMaps: true,
+  poweredByHeader: false,
+};
 
 export default nextConfig;
